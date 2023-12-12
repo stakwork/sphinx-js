bcfbe2e6279b509325b4ea1687c5199a9aea9993
Dylan Bathurst <dylanbathurst@gmail.com>
2023-12-12 07:41:58 -0800

    feat(splash): add splash screen and tailwindcss

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,9 @@
 <!-- src/App.svelte -->
 <script lang="ts">
+  import SbLogo from './assets/second-brain.svelte'
+  import Plus from './assets/plus.svelte'
+  import Button from './components/Button.svelte'
+
   chrome.runtime.sendMessage({
     type: "SPINX_POPUP",
     text: 'Hello from the popup!',
@@ -8,24 +12,15 @@
   let name = 'Sphinx';
 </script>
 
-<main>
-  <img class="logo" src="static/icon.png" alt="Sphinx logo" />
-  <h1>Hello {name}!</h1>
+<main class="flex flex-col font-sans h-[430px] w-[320px] bg-[#23252F]">
+  <header class="bg-[#1C1E26] border-b border-black/25 p-4">
+    <SbLogo />
+  </header>
+  <content class="flex flex-1 flex-col px-5">
+    <div class="flex flex-col text-center pt-20">
+      <h1 class="text-[#F5F5F5] text-xl text-center mb-9">Hello! 👋</h1>
+      <p class="mb-11 text-sm font-normal">Add your first SecondBrain<br /> by entering the URL below and<br /> pay 10 sats to contribute content!</p>
+      <Button on:click={()=>console.log("Clicked")}><Plus /> Add a Second Brain</Button>
+    </div>
+  </content>
 </main>
-
-<style>
-  main {
-    text-align: center;
-    margin: 1em;
-  }
-
-  .logo {
-    height: 3rem;
-  }
-
-  h1 {
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-</style>
---
+++ src/components/Button.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    export let variant: string;
+</script>
+
+<button class={`flex justify-center items-center gap-1 p-3 rounded-md text-sm font-semibold ${variant === 'primary' ? 'bg-[#618AFF]' : 'bg-white'} `} on:click>
+    <slot/>
+</button>
+
