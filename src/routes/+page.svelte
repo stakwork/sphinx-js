b8f0478a4bbae6e6c502bc79501819c3d59cc2a6
Dylan Bathurst <dylanbathurst@gmail.com>
2024-01-31 23:36:42 -0800

    feat(brains): list of brands check to homescreen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,39 @@
 <!-- src/App.svelte -->
 <script lang="ts">
-  // import Empty from "./routes/Empty.svelte";
-  // import AddForm from "./routes/AddBrain/AddForm.svelte";
-
-  // chrome.runtime.sendMessage({
-  //   type: "SPINX_POPUP",
-  //   text: 'Hello from the popup!',
-  // }).then((response) => {console.log(response)});
+  import { onMount } from 'svelte';
+  import Button from "$lib/Button.svelte";
+  import Plus from "$lib/assets/plus.svg";
 
   let name = 'Sphinx';
+  let secondBrainList = [];
+
+  onMount(() => {
+    const bitcoinBrain = {"app_version":null,"description":"Learn about changes in Bitcoin","mission_statement":"Learn about changes in Bitcoin","search_term":"Bitcoin","title":"Bitcoin Graph"}
+    // chrome.storage.sync.set({ brainsList: JSON.stringify([bitcoinBrain])}).then(() => {
+    //   console.log("Value is set");
+    // });
+    chrome.storage.sync.get("brainsList").then((result) => {
+      secondBrainList = JSON.parse(result.brainsList);
+    });
+  
+  });
 </script>
 
-<a href="/add-brain" class="flex items-center justify-center flex-1">
-  <span class="text-[#F5F5F5] text-xl text-center mb-9">Add a Second Brain</span>
-</a>
+<div class="flex flex-1 flex-col justify-center">
+  {#each secondBrainList as secondBrain}
+    <h1>{secondBrain.title}!</h1>
+  {/each}
+
+  {#if secondBrainList.length === 0}
+    <h2 class="text-xl leading-6 text-white mb-8 text-center">Hello! 👋</h2>
+    <p class="text-md text-white mb-11 text-center">
+      Add your first SecondBrain by entering the URL below and pay 10 sats to contribute content! Add a Second Brain
+    </p>
+    <a href="/add-brain" class="self-center">
+      <Button><img alt="The project logo" src={Plus} /> Add a Second Brain</Button>
+    </a>
+  {/if}
+</div>
 
 <style lang="postcss">
   :global(html) {
--- src/routes/add-brain/+page.svelte
+++ src/routes/add-brain/+page.svelte
@@ -4,7 +4,9 @@
   import AddSb from '$lib/assets/add-second-brain.svg'
 
   let url = '';
-
+  const handleTextChange = (event) => {
+    console.log(event.target.value)
+  }
 </script>
 
 <div class="flex flex-col text-center pt-20">
@@ -12,8 +14,8 @@
   <h1 class="text-[#F5F5F5] text-xl text-center mb-9">Add a Second Brain</h1>
 
   <div class="flex flex-col justify-between gap-6">
-  <input class="bg-bg-3 p-4 focus-within:outline-primaryBlue focus-within:outline-2" bind:value={url} placeholder="Paste your URL here..." />
-  <Button disabled class="mt-4 disabled:bg[#52566E]"><img alt="The project logo" src={Plus} /> Add a Second Brain</Button>
-  <Button class="mt-4 bg-transparent border border-white/50">Cancel</Button>
+    <input on:input={handleTextChange} bind:value={url} class="bg-bg-3 p-4 focus-within:outline-primaryBlue focus-within:outline-2" placeholder="Paste your URL here..." />
+    <Button disabled class="mt-4 disabled:bg[#52566E]"><img alt="The project logo" src={Plus} /> Add a Second Brain</Button>
+    <Button class="mt-4 bg-transparent border border-white/50">Cancel</Button>
   </div>
 </div>
